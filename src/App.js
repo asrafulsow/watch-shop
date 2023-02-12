8ee0e8eec7736b8b790c2cad75952142a5f34227
asrafulsow <aisowrav11@gmail.com>
2023-02-12 19:26:39 +0600

    about us added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home/Home';
 import Banner from './components/Home/Banner/Banner';
 import Categories from './components/Home/Categories/Categories';
+import AboutUs from './components/AboutUs/AboutUs';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path='/home' element={<Home />} />
           <Route path='/banner' element={<Banner />} />
           <Route path='/categoris' element={<Categories />} />
+          <Route path='/about' element={<AboutUs />} />
 
         </Routes>
       </Router>
